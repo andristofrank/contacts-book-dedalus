566cddfbc82a27dfb3619cae156cd53610af0eec
andristofrank <andrei.tofan8@gmail.com>
2021-02-24 21:48:44 +0100

    Add contact component implemented
    add contact component is using reactive forms which onSubmit is calling to dispatch addContact action

--- src/app/contact/contact.module.ts
+++ src/app/contact/contact.module.ts
@@ -3,13 +3,16 @@ import { CommonModule } from '@angular/common';
 import { ContactsBookComponent } from './contacts-book/contacts-book.component';
 import { StoreModule } from '@ngrx/store';
 import { contactFeatureKey, reducer } from './store/reducer/contact.reducer';
+import { ContactAddComponent } from './contact-add/contact-add.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
 @NgModule({
-  declarations: [ContactsBookComponent],
+  declarations: [ContactsBookComponent, ContactAddComponent],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     StoreModule.forFeature(contactFeatureKey, reducer)
   ],
   exports: [ContactsBookComponent]
