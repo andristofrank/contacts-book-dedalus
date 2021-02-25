9f2760a8c6fa9cf3679e541904e0855c29155bb4
andristofrank <andrei.tofan8@gmail.com>
2021-02-25 15:38:14 +0100

    Completed initial tests for ngrx state management
    The main focus was on testing the ngrx reducers

--- src/app/contact/store/reducer/contact.reducer.spec.ts
+++ src/app/contact/store/reducer/contact.reducer.spec.ts
@@ -1,13 +1,40 @@
-import { reducer, initialState } from './contact.reducer';
+import { addContact, selectContact } from '../action/contact.actions';
+import * as fromReducer from './contact.reducer';
 
 describe('Contact Reducer', () => {
   describe('an unknown action', () => {
-    it('should return the previous state', () => {
-      const action = {} as any;
+    it('should return the default state', () => {
+      const { initialState } = fromReducer;
+      const action = {
+        type: 'Unknown',
+      };
 
-      const result = reducer(initialState, action);
+      const resultState = fromReducer.reducer(initialState, action);
 
-      expect(result).toBe(initialState);
+      expect(resultState).toBe(initialState);
     });
   });
+
+  describe('addContact action', () => {
+    it('should add a new contact and update the state in an immutable way', () => {
+      const { initialState } = fromReducer;
+      const contact = {firstName: 'Elena', lastName: 'Somescu', phone: '2424325', address: 'Marsalle 37', email: 'brew@br.dk'};
+      const action = addContact(contact);
+      const resultState = fromReducer.reducer(initialState, action);
+
+      expect(resultState).not.toBe(initialState);
+      expect(resultState.contacts.length).toEqual(2);
+    })
+  })
+
+  describe('selectContact action', () => {
+    it('should select a contact from the list of registered contacts', () => {
+      const { initialState } = fromReducer;
+      const index = 1;
+      const action = selectContact({index});
+      const resultState = fromReducer.reducer(initialState, action);
+      expect(resultState.contacts[0]).toEqual(initialState.contacts[0]);
+    })
+  })
 });
+
