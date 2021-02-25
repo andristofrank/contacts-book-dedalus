c88c6c81d01a6cf7e129b9e7eec3975318ed1efe
andristofrank <andrei.tofan8@gmail.com>
2021-02-25 11:01:07 +0100

    declaring and assigning selected contact from store

--- src/app/contact/contact-details/contact-details.component.ts
+++ src/app/contact/contact-details/contact-details.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { select } from '@ngrx/store';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { Contact } from 'src/app/models/contact';
+import { ContactsBookState } from '../store/reducer/contact.reducer';
+import { selectContact } from '../store/selector/contact.selectors';
 
 @Component({
   selector: 'app-contact-details',
@@ -6,8 +12,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-details.component.scss']
 })
 export class ContactDetailsComponent implements OnInit {
+  contact$: Observable<Contact>;
 
-  constructor() { }
+  constructor(private store: Store<ContactsBookState>) { 
+    this.contact$ = this.store.pipe(select(selectContact));
+  }
 
   ngOnInit(): void {
   }
